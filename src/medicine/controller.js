fe4014e39a32228a021c677ee8c2ebe85cf20093
Saksham Mittal <saksham2005mittal@gmail.com>
2024-07-24 14:05:56 +0530

    Completed CRUD on Medicine model

--- src/medicine/controller.js
+++ src/medicine/controller.js
@@ -19,3 +19,71 @@ exports.getMedicineById = async (req, res) => {
         res.status(500).json({ message: "Internal server error." });
     }
 };
+
+exports.createMedicine = async (req, res) => {
+    try {
+        const medicine = new Medicine(req.body);
+        const savedMedicine = await medicine.save();
+        res.json(savedMedicine);
+    } catch (err) {
+        if (err.name === "ValidationError") {
+            const errors = Object.values(err.errors).map((error) => error.message);
+            res.status(400).json({ message: "Invalid creation parameters", ...errors });
+            return;
+        }
+        console.log(err);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
+
+exports.updateMedicineById = async (req, res) => {
+    try {
+        let updatedMedicine;
+        if (req.method === "PUT") {
+            updatedMedicine = await Medicine.findByIdAndUpdate(req.params.id, req.body, {
+                new: true, runValidators: true
+            });
+        } else if (req.method === "PATCH") {
+            updatedMedicine = await Medicine.findByIdAndUpdate(req.params.id, {
+                $set: req.body}, { new: true, runValidators: true });
+        } else {
+            res.status(405).json({ message: "Method not allowed" });
+            return;
+        }
+        if (!updatedMedicine) {
+            res.status(404).json({ message: `Medicine : ${req.params.id} was not found.` });
+            return;
+        }
+        res.json(updatedMedicine);
+    } catch (err) {
+        if (err.name === "CastError" && err.kind === "ObjectId") {
+            res.status(400).json({ message: "Invalid Medicine ID format." });
+            return;
+        }
+        if (err.name === "ValidationError") {
+            const errors = Object.values(err.errors).map((error) => error.message);
+            res.status(400).json({ message: "Invalid creation parameters", ...errors });
+            return;
+        }
+        console.log(err);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
+
+exports.deleteMedicineById = async (req, res) => {
+    try {
+        const deletedMedicine = await Medicine.findByIdAndDelete(req.params.id);
+        if (!deletedMedicine) {
+            res.status(404).json({ message: `Medicine : ${req.params.id} was not found.` });
+            return;
+        }
+        res.status(204).end();
+    } catch (err) {
+        if (err.name === "CastError" && err.kind === "ObjectId") {
+            res.status(400).json({ message: "Invalid Medicine ID format." });
+            return;
+        }
+        console.log(err);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
--- src/medicine/routes.js
+++ src/medicine/routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
 
+// CRUD Routes
 router.get("/:id", controller.getMedicineById);
+router.post("/:id", controller.createMedicine);
+router.put("/:id", controller.updateMedicineById);
+router.patch("/:id", controller.updateMedicineById);
+router.delete("/:id", controller.deleteMedicineById);
 
 module.exports = router;
